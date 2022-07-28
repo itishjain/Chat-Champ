1bfe7c6329c1a8e2edda1692d69e6919ce6adade
itishjain <itish.jain2301@gmail.com>
2022-07-28 18:17:08 +0530

    Changes made to profile.context.js and added authUnsub variable

--- src/App.js
+++ src/App.js
@@ -7,17 +7,20 @@ import PrivateRoute from "./components/PrivateRoute";
 import Home from "./pages/Home";
 import { Switch } from "react-router";
 import PublicRoute from "./components/PublicRoute";
+import { ProfileProvider } from "./context/profile.context";
 
 function App() {
   return (
-    <Switch>
-      <PublicRoute path="/signin">
-        <SignIn />
-      </PublicRoute>
-      <PrivateRoute path="/">
-        <Home />
-      </PrivateRoute>
-    </Switch>
+    <ProfileProvider>
+      <Switch>
+        <PublicRoute path="/signin">
+          <SignIn />
+        </PublicRoute>
+        <PrivateRoute path="/">
+          <Home />
+        </PrivateRoute>
+      </Switch>
+    </ProfileProvider>
   );
 }
 
--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,11 +1,29 @@
 import React from "react";
+import { Container, Loader } from "rsuite";
 import { Redirect, Route } from "react-router";
+import { useProfile } from "../context/profile.context";
 
 const PrivateRoute = ({ children, ...routeProps }) => {
   // As we donot have any profile we will create a boolean variable and set it to False
-  const profile = false; //  Always on sign in page due to false
+  // const profile = false; //  Always on sign in page due to false
 
-  if (!profile) {
+  const { profile, isLoading } = useProfile();
+
+  if (isLoading && !profile) {
+    return (
+      <Container>
+        <Loader
+          center
+          vertical
+          size="md"
+          content="Loading"
+          speed="slow"
+        ></Loader>
+      </Container>
+    );
+  }
+
+  if (!profile && !isLoading) {
     // If we dont have profile we will redirect to sign
     return <Redirect to="/signin" />;
   }
