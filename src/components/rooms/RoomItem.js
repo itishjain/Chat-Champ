ea370db9fbddab0f099ee6cfe586809272671592
itishjain <itish.jain2301@gmail.com>
2022-07-30 18:56:39 +0530

    Added functionality of giving page not found on random url

--- src/components/rooms/RoomItem.js
+++ src/components/rooms/RoomItem.js
@@ -1,12 +1,16 @@
 import React from "react";
-// import TimeAgo from "timeago-react";
+import TimeAgo from "timeago-react";
 
-const RoomItem = () => {
+const RoomItem = ({ room }) => {
+  const { createdAt, name } = room;
+
   return (
     <div>
       <div className="d-flex justify-content-between align-items-center">
-        <h3 className="text-disappear">Room Name</h3>
-        <span>{/* <TimeAgo datetime={new Date()} /> */}</span>
+        <h3 className="text-disappear">{name}</h3>
+        <span>
+          <TimeAgo datetime={new Date(createdAt)} />
+        </span>
       </div>
 
       <div className="d-flex align-items-center text-black-70">
