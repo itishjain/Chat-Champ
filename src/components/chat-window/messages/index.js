286cc874f0d31fc0266a44b272034d3829e10bca
itishjain <itish.jain2301@gmail.com>
2022-08-01 13:14:47 +0530

    Created messages to display on to screen

--- src/components/chat-window/messages/index.js
+++ src/components/chat-window/messages/index.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { database } from "../../../misc/firebase";
+import { transformToArrayWithId } from "../../../misc/helpers";
+import MessageItem from "./MessageItem";
 
-const index = () => {
-  return <div>index messages</div>;
+const Messages = () => {
+  const { chatId } = useParams();
+
+  const [messages, setMessages] = useState(null);
+
+  const isChatEmpty = messages && messages.length === 0;
+  const canShowMessages = messages && messages.length > 0;
+
+  useEffect(() => {
+    const messagesRef = database.ref(`/messages`);
+
+    messagesRef
+      .orderByChild(`roomId`)
+      .equalTo(chatId)
+      .on("value", (snap) => {
+        const data = transformToArrayWithId(snap.val());
+
+        setMessages(data);
+      });
+
+    return () => {
+      messagesRef.off("value");
+    };
+  }, [chatId]);
+
+  return (
+    <ul className="msg-list custom-scroll">
+      {isChatEmpty && <li>No Messages Yet</li>}
+      {canShowMessages &&
+        messages.map((msg) => <MessageItem key={msg.id} message={msg} />)}
+    </ul>
+  );
 };
 
-export default index;
+export default Messages;
